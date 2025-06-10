3ee641bd7bbaf23bc29e51980237c9b0e658206c
Morer11 <151632961+Morer11@users.noreply.github.com>
2025-06-10 13:53:25 +0100

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,34 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cotoapp.webapp',
+  appName: 'COTOAPP',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  android: {
+    allowMixedContent: true,
+    captureInput: true,
+    webContentsDebuggingEnabled: true
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#3b82f6",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    }
+  }
+};
+
+export default config;
